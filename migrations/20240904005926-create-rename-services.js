2185c0197f7749f79983b1ddfcb7ecb897e75e27
Felix <fedecraft3312@gmail.com>
2024-09-03 22:49:19 -0300

    Migrations

---
+++ migrations/20240904005926-create-rename-services.js
@@ -0,0 +1,10 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	async up(queryInterface, Sequelize) {
+		await queryInterface.renameTable('Services', 'service');
+	},
+	async down(queryInterface, Sequelize) {
+		await queryInterface.renameTable('service', 'Services');
+	}
+};
