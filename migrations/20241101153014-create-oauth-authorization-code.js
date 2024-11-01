cfdeb98de48738c88a1c059d5483751c13105baa
Felix <fedecraft3312@gmail.com>
2024-11-01 12:39:05 -0300

    Create oauth authorization code

---
+++ migrations/20241101153014-create-oauth-authorization-code.js
@@ -0,0 +1,66 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+		await queryInterface.createTable("oauth-authorization-code", {
+			id: {
+				type: Sequelize.BIGINT,
+				primaryKey: true,
+				allowNull: false,
+				autoIncrement: true,
+			},
+			authorizationCode: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: "Authorization code is required",
+					},
+				},
+			},
+			expiresAt: {
+				type: Sequelize.DATE,
+			},
+			redirectUri: {
+				type: Sequelize.STRING,
+			},
+			scope: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				validate: {
+					notEmpty: {
+						msg: "Scope is required",
+					},
+				},
+			},
+			userId: {
+				type: Sequelize.BIGINT,
+				references: {
+					model: "user",
+					key: "id",
+				},
+				onUpdate: "CASCADE",
+				onDelete: "CASCADE",
+			},
+			clientId: {
+				type: Sequelize.STRING,
+				references: {
+					model: "oauth2-client",
+					key: "clientId",
+				},
+				onUpdate: "CASCADE",
+				onDelete: "CASCADE",
+			},
+			createdAt: {
+				type: Sequelize.DATE,
+			},
+			updatedAt: {
+				type: Sequelize.DATE,
+			},
+		});
+	},
+	down: async (queryInterface, Sequelize) => {
+		await queryInterface.dropTable("oauth-authorization-code");
+	},
+};
