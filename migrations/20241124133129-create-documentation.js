4a62c183046c5691b0b5a8f99ae48c2e410699c7
Felix Riddle <fedecraft3312@gmail.com>
2024-11-24 10:47:20 -0300

    Documentation model migration

---
+++ migrations/20241124133129-create-documentation.js
@@ -0,0 +1,48 @@
+module.exports = {
+	up: async (queryInterface, Sequelize) => {
+    const TABLE_NAME = "documentation";
+		await queryInterface.createTable(
+			TABLE_NAME,
+			{
+				id: {
+					type: Sequelize.BIGINT,
+					allowNull: false,
+					primaryKey: true,
+					autoIncrement: true,
+				},
+				title: {
+					type: Sequelize.STRING,
+					allowNull: false,
+				},
+				content: {
+					type: Sequelize.TEXT,
+					allowNull: false,
+				},
+				url: {
+					type: Sequelize.STRING,
+					allowNull: false,
+					unique: true,
+				},
+				language: {
+					type: Sequelize.STRING,
+					allowNull: false, // e.g., "en", "es", "fr"
+				},
+				// Mysql doesn't has support for arrays
+				tags: {
+					type: Sequelize.JSON,
+				},
+				requiredRoles: {
+					type: Sequelize.JSON,
+				},
+			},
+			{
+				timestamps: false,
+				tableName: TABLE_NAME,
+				modelName: TABLE_NAME,
+			}
+		);
+	},
+	down: async (queryInterface) => {
+		await queryInterface.dropTable("documentation");
+	},
+};
