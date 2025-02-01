830d11a92092336aab5cee112f1bd70c1f6eb627
Felix Riddle <fedecraft3312@gmail.com>
2025-02-01 14:14:59 -0300

    .

--- migrations/20250126142921-create-configuration.js
+++ migrations/20250126142921-create-configuration.js
@@ -2,27 +2,35 @@ const TABLE_NAME = "configuration";
 
 module.exports = {
 	up: async (queryInterface, Sequelize) => {
-		await queryInterface.createTable(TABLE_NAME, {
-			key: {
-				type: Sequelize.STRING,
-				primaryKey: true,
-				allowNull: false,
-				validate: {
-					notEmpty: {
-						msg: "Key is required",
+		await queryInterface.createTable(
+			TABLE_NAME,
+			{
+				key: {
+					type: Sequelize.STRING,
+					primaryKey: true,
+					allowNull: false,
+					validate: {
+						notEmpty: {
+							msg: "Key is required",
+						},
+					},
+				},
+				value: {
+					type: Sequelize.TEXT,
+					allowNull: false,
+					validate: {
+						notEmpty: {
+							msg: "Value is required",
+						},
 					},
 				},
 			},
-			value: {
-				type: Sequelize.TEXT,
-				allowNull: false,
-				validate: {
-					notEmpty: {
-						msg: "Value is required",
-					},
-				},
-			},
-		});
+			{
+				timestamps: false,
+				tableName: TABLE_NAME,
+				modelName: TABLE_NAME,
+			}
+		);
 	},
 	down: async (queryInterface, Sequelize) => {
 		await queryInterface.dropTable(TABLE_NAME);
--- migrations/20250126191137-create-app-config.js
+++ migrations/20250126191137-create-app-config.js
@@ -3,58 +3,66 @@
 const TABLE_NAME = "app-config";
 module.exports = {
 	async up(queryInterface, Sequelize) {
-		await queryInterface.createTable(TABLE_NAME, {
-			appName: {
-				type: Sequelize.STRING,
-				allowNull: false,
-				primaryKey: true,
-				validate: {
-					notEmpty: {
-						msg: "Application name is required",
+		await queryInterface.createTable(
+			TABLE_NAME,
+			{
+				appName: {
+					type: Sequelize.STRING,
+					allowNull: false,
+					primaryKey: true,
+					validate: {
+						notEmpty: {
+							msg: "Application name is required",
+						},
+					},
+					references: {
+						model: "app",
+						key: "name",
 					},
 				},
-				references: {
-					model: "app",
-					key: "name",
+				configKey: {
+					type: Sequelize.STRING,
+					allowNull: false,
+					primaryKey: true,
+					validate: {
+						notEmpty: {
+							msg: "Configuration key is required",
+						},
+					},
 				},
-			},
-			configKey: {
-				type: Sequelize.STRING,
-				allowNull: false,
-				primaryKey: true,
-				validate: {
-					notEmpty: {
-						msg: "Configuration key is required",
+				configValue: {
+					type: Sequelize.TEXT,
+					allowNull: false,
+				},
+				type: {
+					type: Sequelize.STRING,
+					allowNull: false,
+					validate: {
+						notEmpty: {
+							msg: "Type is required",
+						},
+					},
+				},
+				description: {
+					type: Sequelize.TEXT,
+					allowNull: true,
+				},
+				updatedBy: {
+					type: Sequelize.STRING,
+					allowNull: false,
+					validate: {
+						notEmpty: {
+							msg: "Updated by is required",
+						},
 					},
 				},
 			},
-			configValue: {
-				type: Sequelize.TEXT,
-				allowNull: false,
-			},
-			type: {
-				type: Sequelize.STRING,
-				allowNull: false,
-				validate: {
-					notEmpty: {
-						msg: "Type is required",
-					},
-				},
-			},
-			description: {
-				type: Sequelize.TEXT,
-				allowNull: true,
-			},
-			updatedBy: {
-				type: Sequelize.STRING,
-				allowNull: false,
-				validate: {
-					notEmpty: {
-						msg: "Updated by is required",
-					},
-				},
-			},
-		});
+			{
+				timestamps: false,
+				tableName: TABLE_NAME,
+				modelName: TABLE_NAME,
+			}
+		);
 	},
 	async down(queryInterface, Sequelize) {
 		await queryInterface.dropTable(TABLE_NAME);
